678e87d51162784b139b823ba87345b2f69a95e9
ehsanasgari <asgari@berkeley.edu>
2017-12-09 08:05:00 +0100

    add classifiers

---
+++ classifier/cross_validation.py
@@ -0,0 +1,54 @@
+import sys
+sys.path.append('../')
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
+from file_utility import FileUtility
+from sklearn.metrics import f1_score,confusion_matrix
+
+class CrossValidator(object):
+    '''
+     The Abstract Cross-Validator
+    '''
+    def __init__(self, X, Y):
+        self.X = X
+        self.Y = Y
+        self.scoring =  {'precision_micro': 'precision_micro', 'precision_macro': 'precision_macro', 'recall_macro': 'recall_macro','recall_micro': 'recall_micro', 'f1_macro':'f1_macro', 'f1_micro':'f1_micro'}
+
+
+class KFoldCrossVal(CrossValidator):
+    '''
+        K-fold cross-validation tuning and evaluation
+    '''
+    def __init__(self, X, Y, folds=10, random_state=1):
+        '''
+        :param X:
+        :param Y:
+        :param folds:
+        :param random_state:
+        '''
+        CrossValidator.__init__(self, X, Y)
+        self.cv = StratifiedKFold(n_splits=folds, shuffle=True, random_state=random_state)
+        self.X = X
+        self.Y = Y
+
+    def tune_and_evaluate(self, estimator, parameters, score='macro_f1', n_jobs=-1, file_name='results'):
+        '''
+        :param estimator:
+        :param parameters:p
+        :param score:
+        :param n_jobs:
+        :param file_name: directory/tuning/classifier/features/
+        :return:
+        '''
+        # greed_search
+        self.greed_search = GridSearchCV(estimator=estimator, param_grid=parameters, cv=self.cv, scoring=self.scoring,
+                                         refit=score, error_score=0, n_jobs=n_jobs)
+
+        label_set=list(set(self.Y))
+        # fitting
+        self.greed_search.fit(X=self.X, y=self.Y)
+        Y_predicted=self.greed_search.best_estimator_.predict(self.X)
+        conf=confusion_matrix(self.Y,Y_predicted,labels=label_set)
+        # save in file
+        FileUtility.save_obj([label_set, conf, self.greed_search.best_score_, self.greed_search.best_estimator_, self.greed_search.cv_results_, self.greed_search.best_params_],file_name)
+
+
---
+++ classifier/random_forest.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.append('../')
+
+from sklearn.svm import LinearSVC, SVC
+from cross_validation import KFoldCrossVal
+from file_utility import FileUtility
+from sklearn.ensemble import RandomForestClassifier
+
+
+class Metagenomic_RF:
+    def __init__(self, X, Y):
+        self.model = RandomForestClassifier(bootstrap=True, criterion='gini',
+            min_samples_split= 2, max_features='auto', min_samples_leaf=1, n_estimators=1000)
+        self.X = X
+        self.Y = Y
+
+    def tune_and_eval(self, results_file,
+                      params=[{"n_estimators": [100, 200, 500, 1000],
+              "criterion": [ "entropy"], #"gini",
+              'max_features': ['sqrt'], # 'auto',
+             'min_samples_split':[5], # 2,5,10
+             'min_samples_leaf':[1]}]):#1,2,5
+        CV = KFoldCrossVal(self.X, self.Y, folds=10)
+        CV.tune_and_evaluate(self.model, parameters=params, score='f1_macro', file_name=results_file + '_RF' , n_jobs=15)
